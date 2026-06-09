4e57ebf3097c582a6c5faf29260b75e41834ae9b
tuyenlhm <tuyenlhm@vng.com.vn>
2026-06-09 20:02:50 +0700

    feat: first step

---
+++ Assets/Hihi/Scripts/NewMonoBehaviorForGhost.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hihi
+{
+    public class NewMonoBehaviorForGhost : MonoBehaviour
+    {
+        public void Init()
+        {
+            Debug.Log(name);
+        }
+    }
+}
